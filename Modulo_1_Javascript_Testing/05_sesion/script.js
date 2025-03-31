dd673791cc5ae236bc6688d43c19d2e5bc798efc
Pablo Eduardo Lopez Ontiveros <plopezo@MEGACABLE.COM.MX>
2025-03-31 09:05:56 -0600

    Archivos clase 5

---
+++ Modulo_1_Javascript_Testing/05_sesion/script.js
@@ -0,0 +1,7 @@
+const titulo = document.getElementById("titulo");
+const parrafos = document.getElementsByClassName("parrafo");
+
+console.log(titulo.textContent);
+
+titulo.textContent = "Bienvenidos a mi página web";
+parrafos.style.color = "blue"
