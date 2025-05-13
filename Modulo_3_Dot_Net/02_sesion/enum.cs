7cb57267b1a1f2217869e4de43f59f801b51a2e1
Pablo Eduardo Lopez Ontiveros <plopezo@megacable.com.mx>
2025-05-13 09:39:56 -0600

    Clase 2 C#

---
+++ Modulo_3_Dot_Net/02_sesion/enum.cs
@@ -0,0 +1,6 @@
+public enum ErrorCode
+{
+    None = 0,
+    NotFound = 404,
+    AccesDenied = 403
+}
--- Proyecto C#/HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ Proyecto C#/HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77603ba21410a37ab4a2e995fd9378f5ecf2bc04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a75f2c16f7a1ea638c6c15aa69716328ea2c67f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
